8c10a46984143e6ee1939dfc4376a8d0cf45fc09
Xerxes Rånby <xerxes@gudinna.com>
2021-01-02 21:00:22 +0100

    Version:

--- Etherdream.java
+++ Etherdream.java
@@ -12,15 +12,15 @@ public class Etherdream implements Runnable {
 
     /**
      * Shorthand method to init a byte array from an char.<br>
-     * Usefull to get a
-     * <code>new byte[] { (byte) 0x01, (byte) 0x02 }</code>
-     * from a <code>0x0102</code>.<br>
+     * Usefull to get a <code>new byte[] { (byte) 0x01, (byte) 0x02 }</code> from a
+     * <code>0x0102</code>.<br>
      * Usage: <code>byte[] array = toBytes(0x0102);</code><br>
      * It works for 2 bytes arrays (char).<br>
      * It works for any multiple of 2 bytes arrays.<br>
      * Ex: <code>byte[] longArray = toBytes(0x0102, 0xAABB, 0x1112)</code>
      * 
-     * @param charVal first int to convert in the resulting array (4 bytes), mandatory
+     * @param charVal   first int to convert in the resulting array (4 bytes),
+     *                  mandatory
      * @param charArray additional ints to add to the array (any more 4 bytes ints)
      * @return the resulting byte array
      */
@@ -44,9 +44,11 @@ public class Etherdream implements Runnable {
      * Usage: <code>byte[] array = toBytes(0x01020304);</code><br>
      * It works for 4 bytes arrays (int).<br>
      * It works for any multiple of 4 bytes arrays.<br>
-     * Ex: <code>byte[] longArray = toBytes(0x01020304, 0xAABBCCDD, 0x11121314)</code>
+     * Ex:
+     * <code>byte[] longArray = toBytes(0x01020304, 0xAABBCCDD, 0x11121314)</code>
      * 
-     * @param intVal first int to convert in the resulting array (4 bytes), mandatory
+     * @param intVal   first int to convert in the resulting array (4 bytes),
+     *                 mandatory
      * @param intArray additional ints to add to the array (any more 4 bytes ints)
      * @return the resulting byte array
      */
@@ -64,17 +66,20 @@ public class Etherdream implements Runnable {
 
     /**
      * Shorthand method to init a byte array from a short.<br>
-     * Usefull to get a
-     * <code>new byte[] { (byte) 0x01, (byte) 0x02 }</code>
-     * from a <code>0x0102</code>.<br>
+     * Usefull to get a <code>new byte[] { (byte) 0x01, (byte) 0x02 }</code> from a
+     * <code>0x0102</code>.<br>
      * Usage: <code>byte[] array = toBytes((short) 0x0102);</code><br>
-     * Warning: If the literal is not cast to short, it will be treated as an int, resulting in the call to the {@link #toBytes(int, int...)} method and generating a 4 bytes buffer instead of a 2 bytes buffer.<br>
+     * Warning: If the literal is not cast to short, it will be treated as an int,
+     * resulting in the call to the {@link #toBytes(int, int...)} method and
+     * generating a 4 bytes buffer instead of a 2 bytes buffer.<br>
      * It works for 2 bytes arrays (short).<br>
      * It works for any multiple of 2 bytes arrays.<br>
      * Ex: <code>byte[] longArray = toBytes(0x0102, 0xAABB, 0x1112)</code>
      * 
-     * @param shortVal first short to convert in the resulting array (2 bytes), mandatory
-     * @param shortArray additional shorts to add to the array (any more 2 bytes shorts)
+     * @param shortVal   first short to convert in the resulting array (2 bytes),
+     *                   mandatory
+     * @param shortArray additional shorts to add to the array (any more 2 bytes
+     *                   shorts)
      * @return the resulting byte array
      */
     public static byte[] toBytes(final short shortVal, final short... shortArray) {
@@ -97,9 +102,11 @@ public class Etherdream implements Runnable {
      * Usage: <code>byte[] array = toBytes(0x0102030405060708L);</code><br>
      * It works for 8 bytes arrays (long).<br>
      * It works for any multiple of 8 bytes arrays.<br>
-     * Ex: <code>byte[] longArray = toBytes(0x0102030405060708L, 0xAABBCCDDEEFF0011L)</code>
+     * Ex:
+     * <code>byte[] longArray = toBytes(0x0102030405060708L, 0xAABBCCDDEEFF0011L)</code>
      * 
-     * @param intVal first long to convert in the resulting array (8 bytes), mandatory
+     * @param intVal   first long to convert in the resulting array (8 bytes),
+     *                 mandatory
      * @param intArray additional longs to add to the array (any more 8 bytes longs)
      * @return the resulting byte array
      */
@@ -144,14 +151,15 @@ public class Etherdream implements Runnable {
     static byte[] concat(Byteable... arrays) {
         byte[][] result = new byte[arrays.length][];
         int i = 0;
-        for(Byteable a:arrays){
-            result[i]=a.bytes();
+        for (Byteable a : arrays) {
+            result[i] = a.bytes();
             i++;
         }
         return concat(result);
     }
 
     final Thread thread;
+
     public Etherdream() {
         thread = new Thread(this);
         thread.start();
@@ -169,16 +177,18 @@ public class Etherdream implements Runnable {
     }
 
     enum State {
-        GET_BROADCAST, KEEP_ALIVE_PING, WRITE_DATA,
+        GET_BROADCAST, KEEP_ALIVE_PING, PREPARE_STREAM, WRITE_DATA, BEGIN_PLAYBACK, IDLE;
     }
 
     // https://ether-dream.com/protocol.html
 
     enum Command {
-        PREPARE_STREAM(0x70), BEGIN_PLAYBACK(0x62), QUEUE_RATE_CHANGE(0x74), WRITE_DATA(0x64), STOP(0x73),
-        EMERGENCY_STOP(0x00), EMERGENCY_STOP_ALTERNATIVE(0xFF), CLEAR_EMERGENCY_STOP(0x63), PING(0x3F),
+        PREPARE_STREAM('p'), BEGIN_PLAYBACK('b'), QUEUE_RATE_CHANGE('q'), WRITE_DATA('d'), STOP('s'),
+        EMERGENCY_STOP(0x00), EMERGENCY_STOP_ALTERNATIVE(0xFF), CLEAR_EMERGENCY_STOP('c'),
+        
+        PING('?') , VERSION('v'),
 
-        ACK_RESPONSE(0x61), NAK_FULL_RESPONSE(0x46), NAK_INVALID_RESPONSE(0x49), NAK_STOPCONDITION_RESPONSE(0x21);
+        ACK_RESPONSE('a'), NAK_FULL_RESPONSE('F'), NAK_INVALID_RESPONSE('i'), NAK_STOPCONDITION_RESPONSE('!');
 
         final byte command;
 
@@ -190,14 +200,23 @@ public class Etherdream implements Runnable {
             return new byte[] { command };
         }
 
+        public byte[] bytes(int lowWaterMark, int pointRate) {
+            byte[] w = concat(bytes(), toBytes((short) lowWaterMark), toBytes(pointRate));
+            System.out.println("w.lenght "+w.length);
+            return w;
+        }
+
         public byte[] bytes(DACPoint p) {
-            return concat(bytes(), new byte[]{(byte)1} , p.bytes());
+            return concat(bytes(), toBytes( (short)1 ), p.bytes());
         }
 
         public byte[] bytes(DACPoint[] p) {
 
             byte[] pb = concat(p);
-            return concat(bytes(), new byte[]{(byte)p.length} , pb);
+            System.out.println("pb.lenght "+pb.length);
+            byte[] w = concat(bytes(), toBytes((short) p.length ), pb);
+            System.out.println("w.length"+w.length);
+            return w;
         }
 
         public byte getCommand() {
@@ -222,16 +241,18 @@ public class Etherdream implements Runnable {
     public class DACPoint implements Byteable {
         byte[] p;
 
-        DACPoint(){
+        DACPoint() {
             p = new byte[18];
         }
 
-        DACPoint(int x, int y, int r, int g, int b){
-            p = toBytes((char)0x0, (char)x, (char)y, (char)r, (char)g, (char)b, (char)0x0, (char)0x0 ,(char)0x0);
+        DACPoint(int x, int y, int r, int g, int b) {
+            p = toBytes((char) 0x0, (char) x, (char) y, (char) r, (char) g, (char) b, (char) 0x0, (char) 0x0,
+                    (char) 0x0);
         }
 
-        DACPoint(int x, int y){
-            p = toBytes((char)0x0, (char)x, (char)y, (char)65536, (char)65536, (char)65536, (char)0x0, (char)0x0 ,(char)0x0);
+        DACPoint(int x, int y) {
+            p = toBytes((char) 0x0, (char) x, (char) y, (char) 65536, (char) 65536, (char) 65536, (char) 0x0,
+                    (char) 0x0, (char) 0x0);
         }
 
         /*
@@ -243,23 +264,21 @@ public class Etherdream implements Runnable {
         }
     }
 
-    public void write(byte[] data){
-        outputBytes = data;
-        state = State.WRITE_DATA;
-    }
-
-
     volatile State state = State.GET_BROADCAST;
-    volatile byte[] outputBytes = {};
+    OutputStream output = null;
+    InputStream input = null;
+
     @Override
     public void run() {
         InetAddress etherdreamAddress = null;
+
+        Socket socket = null;
+
+        boolean readTwice = false;
+
         while (true) {
             System.out.println("state " + state);
             try {
-                try{
-                Thread.sleep(500);
-                } catch (InterruptedException e){}
                 switch (state) {
                     case GET_BROADCAST: {
                         // Wait and get broadcast using UDP
@@ -283,7 +302,24 @@ public class Etherdream implements Runnable {
                             etherdreamAddress = response.getAddress();
 
                             if (etherdreamAddress != null) {
+                                if (socket != null) {
+                                    try {
+                                        socket.close();
+                                        socket = null;
+                                    } catch (IOException e1) {
+                                        // TODO Auto-generated catch block
+                                        e1.printStackTrace();
+                                    }
+                                }
+                                socket = new Socket(etherdreamAddress, 7765);
+                                
+                                output = socket.getOutputStream();
+                                input = socket.getInputStream();
                                 state = State.KEEP_ALIVE_PING;
+                                
+                                // When a host first connects to the device, the device immediately sends it a status reply, as if the host had sent a ping packet 
+                                byte[] dac_status = input.readNBytes(22);
+                                System.out.println(ByteFormatter.byteArrayToHexString(dac_status));        
                             }
 
                         }
@@ -292,89 +328,84 @@ public class Etherdream implements Runnable {
                     }
                     case KEEP_ALIVE_PING: {
                         // Send ping using TCP port 7765
+                        Command cmd = Command.PING;
+                        output.write(cmd.bytes());
+                        output.flush();
+                        /*
+                         * struct dac_response { uint8_t response; uint8_t command; struct status
+                         * dac_status; };
+                         *
+                         * struct dac_status { uint8_t protocol; uint8_t light_engine_state; uint8_t
+                         * playback_state; uint8_t source; uint16_t light_engine_flags; uint16_t
+                         * playback_flags; uint16_t source_flags; uint16_t buffer_fullness; uint32_t
+                         * point_rate; uint32_t point_count; };
+                         */
 
-                        try (Socket socket = new Socket(etherdreamAddress, 7765)) {
+                        byte[] dac_response = input.readNBytes(22);
 
-                            Command cmd = Command.PING;
-                            OutputStream output = socket.getOutputStream();
-                            InputStream input = socket.getInputStream();
-                            output.write(cmd.bytes());
+                        System.out.println(ByteFormatter.byteArrayToHexString(dac_response));
 
-                            /*
-                             * struct dac_response { uint8_t response; uint8_t command; struct status
-                             * dac_status; };
-                             */
-
-                            byte[] dac_response = input.readNBytes(3);
-
-                            System.out.println(ByteFormatter.byteArrayToHexString(dac_response));
-
-                            // make sure we got an ACK
-                            if (dac_response[0] != Command.ACK_RESPONSE.command) {
-                                state = State.GET_BROADCAST;
-                                break;
-                            }
-
-                            // make sure we got the response form the PING command
-                            if (dac_response[1] != cmd.command) {
-                                state = State.GET_BROADCAST;
-                                break;
-                            }
-
-                            /*
-                             * struct dac_status { uint8_t protocol; uint8_t light_engine_state; uint8_t
-                             * playback_state; uint8_t source; uint16_t light_engine_flags; uint16_t
-                             * playback_flags; uint16_t source_flags; uint16_t buffer_fullness; uint32_t
-                             * point_rate; uint32_t point_count; };
-                             */
-                            byte[] dac_status = input.readNBytes(20);
-
+                        // make sure we got an ACK
+                        if (dac_response[0] != Command.ACK_RESPONSE.command) {
+                            state = State.GET_BROADCAST;
+                            break;
                         }
-                        write(Command.WRITE_DATA.bytes(new DACPoint[]{new DACPoint(10,10),new DACPoint(-10,-10)}));
+
+                        // make sure we got the response form the PING command
+                        if (dac_response[1] != cmd.command) {
+                            state = State.GET_BROADCAST;
+                            break;
+                        }
+
+                        
+                        output.write(Command.VERSION.bytes());
+                        output.flush();
+                        byte[] version = input.readNBytes(32);
+                        String versionString = new String(version).replace("\0","").strip();
+                        System.out.println("Version: "+versionString);
+                        
+                        output.write(Command.CLEAR_EMERGENCY_STOP.bytes());
+                        output.flush();
+                        dac_response = input.readNBytes(22);
+                        System.out.println(ByteFormatter.byteArrayToHexString(dac_response));
+
+
+                        state = State.PREPARE_STREAM;
+                        break;
+                    }
+                    case PREPARE_STREAM: {
+                        output.write(Command.PREPARE_STREAM.bytes());
+                        output.flush();
+                        byte[] dac_status = input.readNBytes(22);
+                        System.out.println(ByteFormatter.byteArrayToHexString(dac_status));
+                        state = State.WRITE_DATA;
                         break;
                     }
                     case WRITE_DATA: {
-
-                        try (Socket socket = new Socket(etherdreamAddress, 7765)) {
-                            OutputStream output = socket.getOutputStream();
-                            InputStream input = socket.getInputStream();
-
-                            System.out.println(ByteFormatter.byteArrayToHexString(outputBytes));
-                            output.write(outputBytes);
-
-                            /*
-                             * struct dac_response { uint8_t response; uint8_t command; struct status
-                             * dac_status; };
-                             */
-
-                            byte[] dac_response = input.readNBytes(3);
-
-                            System.out.println(ByteFormatter.byteArrayToHexString(dac_response));
-
-                            // make sure we got an ACK
-                            if (dac_response[0] != Command.ACK_RESPONSE.command) {
-                                state = State.GET_BROADCAST;
-                                break;
-                            }
-
-                            // make sure we got the response form the PING command
-                            if (dac_response[1] != outputBytes[0]) {
-                                state = State.GET_BROADCAST;
-                                break;
-                            }
-
-                            /*
-                             * struct dac_status { uint8_t protocol; uint8_t light_engine_state; uint8_t
-                             * playback_state; uint8_t source; uint16_t light_engine_flags; uint16_t
-                             * playback_flags; uint16_t source_flags; uint16_t buffer_fullness; uint32_t
-                             * point_rate; uint32_t point_count; };
-                             */
-                            byte[] dac_status = input.readNBytes(20);
-
-                        }
-
+                        output.write(Command.WRITE_DATA.bytes(getFrame()));
+                        output.flush();
+                        System.out.println("written");
+                        output.flush();
+                        
+                        byte[] dac_status = input.readNBytes(22);
+                        System.out.println(ByteFormatter.byteArrayToHexString(dac_status));
+                        System.out.println("read response");
+                        state = State.BEGIN_PLAYBACK;
                         break;
                     }
+                    case BEGIN_PLAYBACK: {
+                        output.write(Command.BEGIN_PLAYBACK.bytes(0, 24000));
+                        output.flush();
+                        byte[] dac_status = input.readNBytes(22);
+                        state = State.PREPARE_STREAM;
+                        break;
+                    }
+                    case IDLE:
+                    output.write(Command.WRITE_DATA.bytes(getFrame()));
+                    output.flush();
+                    byte[] dac_status = input.readNBytes(22);
+
+                    break;
                     default:
                         state = State.GET_BROADCAST;
                 }
@@ -383,10 +414,19 @@ public class Etherdream implements Runnable {
                  * If any IO error occour for any reason such as network cable disconnect then
                  * try locate the Etherdream DAC again
                  */
+                
                 state = State.GET_BROADCAST;
-                System.out.println(e);
+                e.printStackTrace();
             }
 
         }
+    }
+
+    DACPoint[] getFrame() {
+        DACPoint[] result = new DACPoint[2000];
+        for(int i = 0; i< 2000 ; i++){
+            result[i]=new DACPoint((int)(65534*Math.sinh(i/24000.0)),(int)(65534*Math.cos(i/24000.0)),10000,0,0);
+        }
+        return result;
     }
 }
